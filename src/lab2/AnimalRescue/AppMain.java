7f9a678c34b2c8aff2922cf8858dc67c91f9b886
Cosmin <cosmin.ciocan@savethechildren.org>
2021-04-01 21:07:51 +0300

    lab 2

---
+++ src/lab2/AnimalRescue/AppMain.java
@@ -0,0 +1,13 @@
+package lab2.AnimalRescue;
+
+public class AppMain {
+
+    public static void main(String[] args) {
+        Dog rex = new Dog();
+        System.out.println(rex.breed);
+        System.out.println(rex.weight);
+        rex.eat(1);
+        System.out.println(rex.weight);
+
+    }
+}
